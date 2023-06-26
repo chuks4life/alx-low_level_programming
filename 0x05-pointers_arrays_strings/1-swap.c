6ad5c88379a1cd093a5f2f25abaa9318063b0a0a
chuks4life <c.ndukauba247@gmail.com>
2023-06-26 03:52:38 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *swap_int of two integers numbers
+ *return int *a, int *ib
+ */
+
+void swap_int(int *a, int *b)
+{
+    int man;
+    man = *a;
+    *a = *b;
+    *b = man;
+}
